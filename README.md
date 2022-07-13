3f131e7f624e17ac2bbb0a49ee4e325bf1311d6c
inozura <nodhzen@gmail.com>
2022-07-13 09:35:01 +0700

    update README and fix bug

---
+++ README.md
@@ -0,0 +1,25 @@
+# Whatnime Mobile App
+
+    Search any anime and manga in here
+    Build with open API JikanAPI
+    React Native Expo with Typescript
+
+<p align="center"><img src="/WhatnimeMobile.png"></p>
+
+## Usage
+
+```bash
+# using yarn or npm
+$ yarn (or `npm install`)
+
+# using pnpm
+$ pnpm install --shamefully-hoist
+```
+
+### Use it
+
+```bash
+# development mode
+$ yarn start (or `npm run start` or `pnpm run start`)
+
+```
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,132 +1,166 @@
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, ScrollView, StatusBar, Image, View, Dimensions } from 'react-native';
-import ListCarousel from '../components/ListCarousel';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  StatusBar,
+  Image,
+  View,
+  Dimensions,
+} from "react-native";
+import ListCarousel from "../components/ListCarousel";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import {getAnimeAiring, getAnimeMovie, getAnimeUpcoming, getPopularAnime, getTopManga, getTopNovels} from '../model/jikanAPI'
-import ListItem from '../components/ListItem';
-import { AntDesign } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import DrawerNavigation from '../navigation/DrawerNavigation'
+import {
+  getAnimeAiring,
+  getAnimeMovie,
+  getAnimeUpcoming,
+  getPopularAnime,
+  getTopManga,
+  getTopNovels,
+} from "../model/jikanAPI";
+import ListItem from "../components/ListItem";
+import { AntDesign } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const LoadingGif = require("../assets/images/LoadingChara.gif");
-const Logo = require('../assets/images/MainLogo.png')
+const Logo = require("../assets/images/MainLogo.png");
 
 interface dataTypeFetched {
-  request_hash: string,
-  request_cached: boolean,
-  request_cache_expiry: number,
+  request_hash: string;
+  request_cached: boolean;
+  request_cache_expiry: number;
   top: Array<{
-    mal_id: string,
-    rank: number,
-    title: string,
-    url: string,
-    image_url: string,
-    type: string,
-    start_date: string,
-    end_date: string,
-    members: string,
-    score: number
-  }>
+    mal_id: string;
+    rank: number;
+    title: string;
+    url: string;
+    image_url: string;
+    type: string;
+    start_date: string;
+    end_date: string;
+    members: string;
+    score: number;
+  }>;
 }
 
 interface dataTypeMangaFetched {
-  request_hash: string,
-  request_cached: boolean,
-  request_cache_expiry: number,
+  request_hash: string;
+  request_cached: boolean;
+  request_cache_expiry: number;
   top: Array<{
-    mal_id: string,
-    rank: number,
-    title: string,
-    url: string,
-    image_url: string,
-    type: string,
-    start_date: string,
-    end_date: string,
-    members: string,
-    score: number
-  }>
+    mal_id: string;
+    rank: number;
+    title: string;
+    url: string;
+    image_url: string;
+    type: string;
+    start_date: string;
+    end_date: string;
+    members: string;
+    score: number;
+  }>;
 }
 
-const Home = ({navigation}: any) => {
-  const [animeUpcoming, setAnimeUpcoming] = useState<dataTypeFetched['top']>([]);
-  const [popularAnime, setPopularAnime] = useState<dataTypeFetched['top']>();
-  const [airingAnime, setAiringAnime] = useState<dataTypeFetched['top']>();
-  const [movieAnime, setMovieAnime] = useState<dataTypeFetched['top']>();
-  const [topManga, setTopManga] = useState<dataTypeMangaFetched['top']>();
-  const [topNovels, setTopNovels] = useState<dataTypeMangaFetched['top']>();
-  const [isLoading, setIsLoading] = useState(false)
+const Home = ({ navigation }: any) => {
+  const [animeUpcoming, setAnimeUpcoming] = useState<dataTypeFetched["top"]>(
+    []
+  );
+  const [popularAnime, setPopularAnime] = useState<dataTypeFetched["top"]>();
+  const [airingAnime, setAiringAnime] = useState<dataTypeFetched["top"]>();
+  const [movieAnime, setMovieAnime] = useState<dataTypeFetched["top"]>();
+  const [topManga, setTopManga] = useState<dataTypeMangaFetched["top"]>();
+  const [topNovels, setTopNovels] = useState<dataTypeMangaFetched["top"]>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getData = async () => {
+    try {
+      setIsLoading(true);
+
+      const dataUpcoming = await getAnimeUpcoming();
+      const dataPopularAnime = await getPopularAnime();
+      const dataAiringAnime = await getAnimeAiring();
+      const dataMovieAnime = await getAnimeMovie();
+      const dataTopManga = await getTopManga();
+      const dataTopNovels = await getTopNovels();
+
+      // set to state
+      setAnimeUpcoming(dataUpcoming.top);
+      setPopularAnime(dataPopularAnime.top);
+      setAiringAnime(dataAiringAnime.top);
+      setMovieAnime(dataMovieAnime.top);
+      setTopManga(dataTopManga.top);
+      setTopNovels(dataTopNovels.top);
+    } catch (error) {
+      console.warn(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        setIsLoading(true)
-
-        const dataUpcoming = await getAnimeUpcoming();
-        const dataPopularAnime = await getPopularAnime();
-        const dataAiringAnime = await getAnimeAiring();
-        const dataMovieAnime = await getAnimeMovie();
-        const dataTopManga = await getTopManga();
-        const dataTopNovels = await getTopNovels();
-
-        // set to state
-        setAnimeUpcoming(dataUpcoming.top);
-        setPopularAnime(dataPopularAnime.top);
-        setAiringAnime(dataAiringAnime.top);
-        setMovieAnime(dataMovieAnime.top);
-        setTopManga(dataTopManga.top);
-        setTopNovels(dataTopNovels.top);
-      } catch (error) {
-        console.warn(error)
-      } finally {
-        setIsLoading(false)
-      }
-    }
+    getData();
 
     return () => {
       setAiringAnime([]);
       setAnimeUpcoming([]);
-      setIsLoading(false);
+      setIsLoading(true);
       setMovieAnime([]);
       setPopularAnime([]);
       setTopManga([]);
       setTopNovels([]);
-    }
-
-    getData();
-  }, [])
+    };
+  }, []);
 
   return isLoading ? (
-    <View style={{flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#1f1f1f"}}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#1f1f1f",
+      }}
+    >
       <Image source={LoadingGif} />
-      <StatusBar 
-          barStyle="light-content"
-        />
+      <StatusBar barStyle="light-content" />
     </View>
   ) : (
     <SafeAreaView style={styles.container}>
       {/* <DrawerNavigation navigation={navigation} /> */}
-      <ScrollView style={{marginBottom: 50}}>
-        <StatusBar 
-          barStyle="light-content"
-        />
+      <ScrollView style={{ marginBottom: 50 }}>
+        <StatusBar barStyle="light-content" />
         <View style={styles.headWrap}>
           <Text style={styles.headTitle}>Whatnime</Text>
-          <Image source={Logo} width={20} height={15} resizeMode="contain" style={styles.logoImage} />
+          <Image
+            source={Logo}
+            width={20}
+            height={15}
+            resizeMode="contain"
+            style={styles.logoImage}
+          />
         </View>
-        <ListCarousel data={animeUpcoming && animeUpcoming.slice(0, 10)} navigation={navigation} />
-        
+        <ListCarousel
+          data={animeUpcoming && animeUpcoming.slice(0, 10)}
+          navigation={navigation}
+        />
+
         {/* Popular ANIME */}
         <View style={styles.containerSection}>
           <View style={styles.headingSecttion}>
             <Text style={styles.titleHeadingSection}>Popular Anime</Text>
             <TouchableOpacity
-              onPress={() => navigation.navigate("ListScreen", {itemType: "anime",title: "Popular Anime",type: "bypopularity"})}
+              onPress={() =>
+                navigation.navigate("ListScreen", {
+                  itemType: "anime",
+                  title: "Popular Anime",
+                  type: "bypopularity",
+                })
+              }
             >
               <AntDesign name="arrowright" size={20} color="white" />
             </TouchableOpacity>
           </View>
-          <ListItem data={popularAnime} navigation={navigation} type="anime"/>
+          <ListItem data={popularAnime} navigation={navigation} type="anime" />
         </View>
 
         {/* Airing ANIME */}
@@ -134,12 +168,23 @@ const Home = ({navigation}: any) => {
           <View style={styles.headingSecttion}>
             <Text style={styles.titleHeadingSection}>Airing Anime</Text>
             <TouchableOpacity
-              onPress={() => navigation.navigate("ListScreen", {itemType: "anime",title: "Airing Anime",type: "airing"})}
+              onPress={() =>
+                navigation.navigate("ListScreen", {
+                  itemType: "anime",
+                  title: "Airing Anime",
+                  type: "airing",
+                })
+              }
             >
               <AntDesign name="arrowright" size={20} color="white" />
             </TouchableOpacity>
           </View>
-          <ListItem data={airingAnime} navigation={navigation} square type="anime"/>
+          <ListItem
+            data={airingAnime}
+            navigation={navigation}
+            square
+            type="anime"
+          />
         </View>
 
         {/* Movie */}
@@ -147,12 +192,18 @@ const Home = ({navigation}: any) => {
           <View style={styles.headingSecttion}>
             <Text style={styles.titleHeadingSection}>Best Movie</Text>
             <TouchableOpacity
-              onPress={() => navigation.navigate("ListScreen", {itemType: "anime",title: "TOP Movie",type: "movie"})}
+              onPress={() =>
+                navigation.navigate("ListScreen", {
+                  itemType: "anime",
+                  title: "TOP Movie",
+                  type: "movie",
+                })
+              }
             >
               <AntDesign name="arrowright" size={20} color="white" />
             </TouchableOpacity>
           </View>
-          <ListItem data={movieAnime} navigation={navigation} type="anime"/>
+          <ListItem data={movieAnime} navigation={navigation} type="anime" />
         </View>
 
         {/* Top Manga */}
@@ -160,18 +211,29 @@ const Home = ({navigation}: any) => {
           <View style={styles.headingSecttion}>
             <Text style={styles.titleHeadingSection}>Top Manga</Text>
             <TouchableOpacity
-              onPress={() => navigation.navigate("ListScreen", {itemType: "manga",title: "Top Manga",type: "manga", subType: "manga"})}
+              onPress={() =>
+                navigation.navigate("ListScreen", {
+                  itemType: "manga",
+                  title: "Top Manga",
+                  type: "manga",
+                  subType: "manga",
+                })
+              }
             >
               <AntDesign name="arrowright" size={20} color="white" />
             </TouchableOpacity>
           </View>
-          <ListItem data={topManga} navigation={navigation} square type="manga"/>
+          <ListItem
+            data={topManga}
+            navigation={navigation}
+            square
+            type="manga"
+          />
         </View>
-
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 export default Home;
 
@@ -180,22 +242,22 @@ const styles = StyleSheet.create({
     flex: 1,
     width: "100%",
     backgroundColor: "#1f1f1f",
-    overflow: "hidden"
+    overflow: "hidden",
   },
   containerLoading: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: "white"
+    fontWeight: "bold",
+    color: "white",
   },
   separator: {
     marginVertical: 10,
     width: 1,
-    height: '80%',
+    height: "80%",
   },
   containerSection: {
     marginVertical: 15,
@@ -207,7 +269,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     width: "100%",
-    backgroundColor: "transparent"
+    backgroundColor: "transparent",
   },
   titleHeadingSection: {
     flexWrap: "wrap",
@@ -219,16 +281,16 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingHorizontal: 15
+    paddingHorizontal: 15,
   },
   logoImage: {
     width: 70,
     height: 70,
-    resizeMode: "contain"
+    resizeMode: "contain",
   },
   headTitle: {
     color: "white",
     fontSize: 27,
     fontFamily: "montserrat-extra-bold",
-  }
+  },
 });
